671e972d52a90d9dd2e464bd5a10c8c01bc9dabd
Hrithik-coder <67430057+Hrithik-coder@users.noreply.github.com>
2022-12-15 19:59:31 +0530

    app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
+import { useState } from "react"
+import "./App.css"
+import CheckBox from "./components/CheckBox"
+import { years } from "./data"
+import YearCard from "./components/YearCard"
 
 function App() {
+  const [checked, setChecked] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header>
+        {years.map((obj) => (
+          <CheckBox setValue={setChecked} key={obj.id} info={obj} />
+        ))}
       </header>
+
+      <div className="cards_container">
+        {checked?.map((id) => (
+          <div key={id}>
+            <YearCard title={years.find((i) => i.id === id).year} />
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
